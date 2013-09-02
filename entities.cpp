939d378522e0dbddb2507a2fa120cf77eaf6bbd9
offset <schakal3000@gmail.com>
2013-09-02 19:49:11 +0200

    Extended Entities with a Player class. An instantiation of this class is going to be used by the player.

--- entities.cpp
+++ entities.cpp
@@ -1,5 +1,27 @@
 #include "entities.h"
 
-entities::entities()
+void Entities::setPosition(sf::Vector2f pos)
 {
+    position = pos;
+}
+
+void Entities::setPosition(float px, float py)
+{
+    position.x = px;
+    position.y = py;
+}
+
+sf::Vector2f Entities::getPosition() const
+{
+    return position;
+}
+
+Player::Player(sf::Vector2f pos) : position(pos), jumpHeight(2), jumpTime(2.f), sf::Rect(pos.x, pos.y, 1.f, 2.f)
+{
+    
+}
+
+Player::Player(float px, float py) : position(px, py), jumpHeight(2), jumpTime(2.f), sf::Rect(pos.x, pos.y, 1.f, 2.f)
+{
+    
 }
--- entities.h
+++ entities.h
@@ -1,15 +1,43 @@
 #ifndef ENTITIES_H
 #define ENTITIES_H
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
 
 /*!
  * \brief The entities class
  *
- * This is a base class for different entities appearing in the game. It only specifies common attributes
+ * This is a base class for different entities appearing in the game. It only specifies common attributes.
  */
-class entities
+class Entities
 {
 public:
-    entities() = 0;
+    Entities();
+    void setPosition(sf::Vector2f pos);
+    void setPosition(float px, float py);
+    sf::Vector2f getPosition() const;
+    
+private:
+    sf::Vector2f position;
+};
+
+class Player : public Entities
+{
+public:
+    // construct the player object at a specific position
+    Player(sf::Vector2f pos);
+    Player(float px, float py);
+    void setJumpTime(float time);
+    float getJumpTime() const;
+    void setJumpHeight(int height);
+    int getJumpHeight() const;
+    
+private:
+    // how high is the player able to jump
+    int jumpHeight;
+    // how long may the player jump
+    float jumpTime;
+    // maybe used in further implementations with other obstacles than falling down (enemies, etc.)
+    sf::Rect hitBox;
 };
 
 #endif // ENTITIES_H
